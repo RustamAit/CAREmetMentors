6d672949e43330db3654855b0e28283db5e88f22
Rustam <rses54@gmail.com>
2019-09-29 11:04:15 +0300

    multiDexEnabled

--- app/src/main/java/kz/caremet/mentors/android_client_app/App.kt
+++ app/src/main/java/kz/caremet/mentors/android_client_app/App.kt
@@ -1,16 +1,21 @@
 package kz.caremet.mentors.android_client_app
 
 import android.app.Application
+import androidx.multidex.MultiDex
+import androidx.multidex.MultiDexApplication
 import kz.caremet.mentors.android_client_app.di.appModules
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
-class App: Application() {
+class App: MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
             modules(appModules)
             androidContext(this@App)
         }
+
+        MultiDex.install(this)
+
     }
 }
